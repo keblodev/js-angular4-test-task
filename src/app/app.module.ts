a1ee390c366b3ff6f28f05100bfe39d97028afc4
Roman Fromrome <romanfromrome@gmail.com>
2017-04-09 12:57:25 +0800

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { 
+  MaterialRootModule,
+  MdProgressBar,
+  MdSpinner
+} from '@angular/material';
+
+import 'hammerjs';
+
+import { routes } from './app.routes';
+
+import { ApiService } from './service/api.service';
+import { RemoteStateService } from './service/remote-state.service';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(
+      routes,
+      {
+        useHash: false
+      }),
+    MaterialRootModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    ApiService,
+    RemoteStateService
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
